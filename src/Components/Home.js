ec45d3c15f8be4eab8854af3a0d83eb37cf38aad
Abdollah <abefarsane@gmail.com>
2023-04-05 18:33:06 +0200

    first push restyled

---
+++ src/Components/Home.js
@@ -0,0 +1,34 @@
+import React, { Component, useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../Tools/Context/AuthContext';
+import SearchBar from '../Tools/sComponents/SearchBar';
+import axios from 'axios';
+import CustomSlider from '../Tools/sComponents/CustomSlider';
+
+export default function Home() {
+
+    const { authState } = useContext(AuthContext)
+
+    return (
+        <div className='home-page'>
+
+
+                <section className='home-img'>
+                    <section className='home-img-over'>
+                    </section>
+                    <section className='home-header'>
+                        <h1>Plan your next trip with us!</h1>
+                    </section>
+                    <section className='search-section'>
+                        <SearchBar />
+                    </section>
+                </section>
+            
+
+
+            <section className='slider'>
+                <CustomSlider />
+            </section>
+            
+        </div>
+    )
+}
